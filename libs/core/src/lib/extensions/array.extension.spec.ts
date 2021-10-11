6c486a96952fcb8585bf603b66edd23c04a0b43d
Riccardo Sale <ricsal@microsoft.com>
2021-10-11 19:43:33 +0200

    added array.merge extension

--- libs/core/src/lib/extensions/array.extension.spec.ts
+++ libs/core/src/lib/extensions/array.extension.spec.ts
@@ -18,4 +18,91 @@ describe('array.extension', () => {
 
     expect(result).toStrictEqual({ k1a: { id: 1, value: 'a' }, k2b: { id: 2, value: 'b' }, k3c: { id: 3, value: 'c' } });
   });
+
+  it('merge - override', () => {
+    const input1: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b', otherProp: 'zz' }
+    ];
+
+    const input2: MergeInput[] = [
+      { id: 2, value: 'b1', otherProp: 'yy' },
+      { id: 3, value: 'c' }
+    ];
+
+    const expectedResult: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b1', otherProp: 'yy' },
+      { id: 3, value: 'c' }
+    ];
+
+    expect(input1.merge(input2, 'id', 'override')).toStrictEqual(expectedResult);
+    expect(input1.merge(input2, a => a.id, 'override')).toStrictEqual(expectedResult);
+  });
+
+  it('merge - keepExisting', () => {
+    const input1: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b', otherProp: 'zz' }
+    ];
+
+    const input2: MergeInput[] = [
+      { id: 2, value: 'b1', otherProp: 'yy' },
+      { id: 3, value: 'c' }
+    ];
+
+    const expectedResult: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b', otherProp: 'zz' },
+      { id: 3, value: 'c' }
+    ];
+
+    expect(input1.merge(input2, 'id', 'keepExisting')).toStrictEqual(expectedResult);
+    expect(input1.merge(input2, a => a.id, 'keepExisting')).toStrictEqual(expectedResult);
+  });
+
+  it('merge - mergeWithAssign', () => {
+    const input1: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b', otherProp: 'zz' }
+    ];
+
+    const input2: MergeInput[] = [
+      { id: 2, value: 'b1' },
+      { id: 3, value: 'c' }
+    ];
+
+    const expectedResult: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b1', otherProp: 'zz' },
+      { id: 3, value: 'c' }
+    ];
+
+    expect(input1.merge(input2, 'id', 'mergeWithAssign')).toStrictEqual(expectedResult);
+    expect(input1.merge(input2, a => a.id, 'mergeWithAssign')).toStrictEqual(expectedResult);
+  });
+
+  it('merge - custom', () => {
+    const input1: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b', otherProp: 'zz' }
+    ];
+
+    const input2: MergeInput[] = [
+      { id: 2, value: 'b1', otherProp: 'yy' },
+      { id: 3, value: 'c' }
+    ];
+
+    const expectedResult: MergeInput[] = [
+      { id: 1, value: 'a' },
+      { id: 2, value: 'b1', otherProp: 'zz_yy' },
+      { id: 3, value: 'c' }
+    ];
+
+    const conflictFn = (i: MergeInput, o: MergeInput) => ({ id: i.id, value: o.value, otherProp: `${i.otherProp}_${o.otherProp}` } as MergeInput);
+    expect(input1.merge(input2, 'id', conflictFn)).toStrictEqual(expectedResult);
+    expect(input1.merge(input2, a => a.id, conflictFn)).toStrictEqual(expectedResult);
+  });
 });
+
+type MergeInput = { id: number, value: string, otherProp?: string };
--- libs/core/src/lib/extensions/array.extension.ts
+++ libs/core/src/lib/extensions/array.extension.ts
@@ -1,3 +1,4 @@
+
 export { };
 
 declare global {
@@ -7,6 +8,12 @@ declare global {
       valueFunc: (item: T) => TValue,
       defaultValue?: { [key: string]: TValue }
     ): { [key: string]: TValue };
+
+    merge(
+      other: ArrayLike<T>,
+      keySelector: keyof T | ((item: T) => unknown),
+      action: 'keepExisting' | 'override' | 'mergeWithAssign' | ((original: T, other: T) => T)
+    ): T[];
   }
 }
 
@@ -23,5 +30,42 @@ Array.prototype.convertToObject = function <T, TValue>(
     );
   } else {
     return defaultValue;
+  }
+};
+
+Array.prototype.merge = function <T>(
+  this: Array<T>,
+  otherArray: T[],
+  keySelector: keyof T | ((item: T) => unknown),
+  conflctAction: 'keepExisting' | 'override' | 'mergeWithAssign' | ((original: T, other: T) => T)
+): T[] {
+  if (this) {
+    const keySelectorPredicate = typeof keySelector === 'function' ? keySelector : ((i: T) => i[keySelector]);
+
+    const list1 = this.map(originalItem => {
+      const originalItemKey = keySelectorPredicate(originalItem);
+      const otherItem = otherArray.find(o => keySelectorPredicate(o) === originalItemKey);
+
+      if (otherItem) {
+        switch (conflctAction) {
+          case 'keepExisting':
+            return originalItem;
+          case 'override':
+            return otherItem;
+          case 'mergeWithAssign':
+            return Object.assign({}, originalItem, otherItem);
+          default:
+            return conflctAction(originalItem, otherItem);
+        }
+      } else {
+        return originalItem;
+      }
+    });
+
+    const list2 = otherArray.filter(o => !list1.some(l => keySelectorPredicate(l) === keySelectorPredicate(o)));
+
+    return [...list1, ...list2];
+  } else {
+    return otherArray;
   }
 };

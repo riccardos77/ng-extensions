28386cae2815deb23a49af058787a0057f813508
Riccardo Sale <riccardos@live.com>
2022-02-27 12:05:57 +0100

    fixed jest config

--- projects/ng-ext/core/jest.config.js
+++ projects/ng-ext/core/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'forms',
+  displayName: 'core',
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/forms',
+  coverageDirectory: '../../../coverage/libs/core',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },

8b5483e124ac114a1e67713c82686002df0fd8a9
Riccardo Sale <riccardos@live.com>
2022-03-14 21:18:33 +0100

    router getActiveComponent: catched errors from rootContexts.getContext

--- projects/ng-ext/router/src/lib/extensions/router.extension.ts
+++ projects/ng-ext/router/src/lib/extensions/router.extension.ts
@@ -10,9 +10,13 @@ declare module '@angular/router' {
 
 Router.prototype.getActiveComponent = function <T>(this: Router): T | undefined {
   if (this !== undefined) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-    const rootContexts = (this as any).rootContexts as ChildrenOutletContexts;
-    return rootContexts.getContext(this.routerState.root.outlet)?.outlet?.component as T;
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      const rootContexts = (this as any).rootContexts as ChildrenOutletContexts;
+      return rootContexts.getContext(this.routerState.root.outlet)?.outlet?.component as T;
+    } catch {
+      return undefined;
+    }
   } else {
     return undefined;
   }
